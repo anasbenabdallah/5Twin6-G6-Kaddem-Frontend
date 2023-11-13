163d51f0ef3ed170091bc1b2365aa62bd9dbc30d
anasbenabdallah <anas.benabdallah@esprit.tn>
2023-11-13 20:46:01 +0100

    Anas Frontend Part get request

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, ContactComponent],
+  imports: [BrowserModule, AppRoutingModule, RouterModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/contract.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ContractService {
+  private apiUrl = 'http://localhost:8089/Kaddem/contrat/retrieve-all-contrats';
+
+  constructor(private http: HttpClient) {}
+
+  getContrats(): Observable<any> {
+    return this.http.get<any>(this.apiUrl);
+  }
+}
