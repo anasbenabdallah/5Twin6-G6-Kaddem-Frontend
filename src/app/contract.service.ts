a2c490eb984b001fff7d530e1e00ab01407d4cbd
anasbenabdallah <anas.benabdallah@esprit.tn>
2023-11-16 21:00:20 +0100

    ip

--- src/app/contract.service.ts
+++ src/app/contract.service.ts
@@ -6,7 +6,8 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class ContractService {
-  private apiUrl = 'http://localhost:8000/Kaddem/contrat/retrieve-all-contrats';
+  private apiUrl =
+    'http://192.168.33.10:8000/Kaddem/contrat/retrieve-all-contrats';
 
   constructor(private http: HttpClient) {}
 
